fa0b906ee1d1994ae13399cc26071b8123b984d2
Sumakmur51017028 <48231289+Sumakmur51017028@users.noreply.github.com>
2019-11-13 20:09:07 +0800

    Add files via upload

---
+++ minus.java
@@ -0,0 +1,46 @@
+
+import javax.swing.*;
+import java.awt.event.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sumakmur Goenawan
+ */
+
+public abstract class minus implements ActionListener {
+    private static void createAndShowGUI(){
+        JFrame frame = new JFrame("hitung mundur"); frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(20,30,300,200);
+        frame.getContentPane().setLayout(null);
+        JButton butt=new JButton("click me");
+        frame.getContentPane().add(butt);
+        butt.setBounds(20,20,200,20);
+        minus app = new minus(){};
+        app.label = new JLabel("clicks =3");
+        app.label.setBounds(40,40,200,20);
+        frame.getContentPane().add(app.label);
+        
+        butt.addActionListener(app);
+        frame.setVisible(true);
+    }
+    public void actionPerformed(ActionEvent e )
+    {
+        clickCount--;
+        label.setText("clicks ="+clickCount);
+    }
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run(){
+                createAndShowGUI();
+            }
+        });
+    }
+    int clickCount=3;
+    JLabel label;
+}
